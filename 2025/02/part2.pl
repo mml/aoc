2ae28c19ceb742c7ad1598c92bc53951e1c410c3
Matthew Liggett <mml@pobox.com>
2025-12-02 06:10:54 -0800

    [2025 02] Part 2

---
+++ 2025/02/part2.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+require 5;
+use warnings;
+use strict;
+
+$/ = ',';
+my $n = 0;
+
+while (<>) {
+  chomp;
+  my($lo, $hi) = split /-/, $_;
+  for (my $id = $lo; $id <= $hi; ++$id) {
+    if ($id =~ /^(\d+)(\1)+$/) {
+      #print "<$id>\n";
+      $n += $id;
+    }
+  }
+}
+
+print "$n\n";
